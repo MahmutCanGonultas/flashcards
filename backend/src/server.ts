fe20ce05530c559ec0c65d626b56b0b1b1a85770
MahmutCan Gonultas <mahmutcangonultas@outlook.com>
2026-06-30 15:32:32 +0300

    feat: add database connection with pg pool

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 
 const app = express();
-
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("What If");
+  res.send("Flashcards API is running 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log("Server started");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
